2e1e6f28f2be21feb2554378571c22306a62a065
Shem <smagnezi8@gmail.com>
2022-06-19 13:22:40 +0300

    Update tags.js

--- backend/routes/api/tags.js
+++ backend/routes/api/tags.js
@@ -4,8 +4,7 @@ var Item = mongoose.model('Item');
 
 // return a list of tags
 router.get('/', function(req, res, next) {
-  console.log("test");
-  Item.find().distinct('tagListX').then(function(tags){
+  Item.find().distincts('tagList').then(function(tags){
     return res.json({tags: tags});
   }).catch(next);
 });
